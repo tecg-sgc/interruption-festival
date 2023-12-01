823faea0082718adae7b666f88175e9ee6c8a609
Toon Van den Bos <toon@whitecube.be>
2023-12-01 11:46:40 +0100

    Added soft deletes

--- models/Artist.php
+++ models/Artist.php
@@ -5,7 +5,7 @@ class Artist extends BaseModel
     public function homepageArtists($edition)
     {
         return $this->get(
-            'SELECT DISTINCT a.name, a.thumb, a.birthday, g.name AS \'genre\', a.youtube, a.popularity FROM `artists` a JOIN `genres` g ON a.genre_id = g.id JOIN `schedules` s ON s.artist_id = a.id JOIN `days` d ON s.day_id = d.id WHERE d.edition_id = :edition_id ORDER BY a.`popularity` DESC LIMIT 3;',
+            'SELECT DISTINCT a.name, a.thumb, a.birthday, g.name AS \'genre\', a.youtube, a.popularity FROM `artists` a JOIN `genres` g ON a.genre_id = g.id JOIN `schedules` s ON s.artist_id = a.id JOIN `days` d ON s.day_id = d.id WHERE d.edition_id = :edition_id AND a.`deleted_at` IS NULL ORDER BY a.`popularity` DESC LIMIT 3;',
             [
                 'edition_id' => $edition->id
             ]);
